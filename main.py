39179595e0071c2ccb1a2c8502fce62b4fdc1449
MesRoto <mikael.roto@gmail.com>
2021-02-02 10:57:52 +0200

    First push

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 from waterer import Watererer
 
-water = Watererer
 if __name__ == "__main__":
-    water.main_loop()
+    water = Watererer
+    water.start()
