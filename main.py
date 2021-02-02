b2af006d112650675d1a6c89b0fb73e7c52e1933
MesRoto <mikael.roto@gmail.com>
2021-02-02 10:56:00 +0200

    First push

--- main.py
+++ main.py
@@ -3,21 +3,6 @@ from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 from waterer import Watererer
 
-@app.route('/success/<name>')
-def success(name):
-    return f'welcome {name}'
-
-
-@app.route("/login", methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
-
-
+water = Watererer
 if __name__ == "__main__":
-    water = Watererer
     water.main_loop()
